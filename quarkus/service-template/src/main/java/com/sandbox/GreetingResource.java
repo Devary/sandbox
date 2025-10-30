ca45eb8d1e18e4df261396f1321705c5193f4647
Fakher H. <hfekher@gmail.com>
2025-10-30 23:27:53 +0100

    Add Swagger support

--- quarkus/service-template/src/main/java/com/sandbox/GreetingResource.java
+++ quarkus/service-template/src/main/java/com/sandbox/GreetingResource.java
@@ -4,12 +4,16 @@ import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/hello")
+@Tag(name = "Greeting", description = "Operations about greetings")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
+    @Operation(summary = "Returns a greeting message", description = "Provides a friendly greeting for the caller.")
     public String hello() {
         return "Hello from Quarkus REST";
     }
